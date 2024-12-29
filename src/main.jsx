bc38adcaa16f5a7ff39db52a611c4ee4d18da16c
noureqo <128331928+noureqo@users.noreply.github.com>
2024-12-29 18:27:47 +0200

    finishing the auth pages(login/sighnup) & 404 page

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,39 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MovieDetails from './pages/MovieDetails'
 import MovieDiscovery from './pages/MovieDiscovery'
+import Auth from './pages/Auth'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import { AuthProvider } from './context/AuthContext'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/global.css'
+import './styles/pages.css'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<MovieDiscovery />} />
-        <Route path="/movie/:id" element={<MovieDetails />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <MovieDiscovery />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/movie/:id"
+            element={
+              <ProtectedRoute>
+                <MovieDetails />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   </React.StrictMode>
 )
